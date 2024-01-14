b17a9801cd54127ab2ad7215431bddd6fed9c990
IgosProjects <148572592+IgosProjects@users.noreply.github.com>
2024-01-14 14:50:43 +0100

    Add files via upload

---
+++ js/tab.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    showTabFromHash();
+
+    window.addEventListener('hashchange', function () {
+        showTabFromHash();
+    });
+});
+
+
+
+function showTabFromHash() {
+    var hash = window.location.hash.substr(1);
+    var tabs = document.querySelectorAll('.tab');
+    var homeTab = document.getElementById('home'); // Replace 'home' with the actual ID of your home tab
+
+    if (!hash || hash === 'home') {  // Show home tab by default
+        // Hide other tabs
+        tabs.forEach(function (tab) {
+            if (tab !== homeTab) {
+                tab.style.display = 'none';
+            }
+        });
+
+        // Show home tab
+        homeTab.style.display = 'block';
+    } else {
+        tabs.forEach(function (tab) {
+            if (tab.id === hash) {
+                tab.style.display = 'block';
+            } else {
+                tab.style.display = 'none';
+            }
+        });
+    }
+}
